80e1c4021b91b146c7074ca512adf483d5ccef4d
gggiovanny <giovanny.gonzalez.19@gmail.com>
2022-09-07 22:22:55 -0500

    fixes prisma commands

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -3,14 +3,20 @@ import { PrismaClient } from '@prisma/client';
 const db = new PrismaClient();
 
 const fixedCosts = [
-  { costName: 'Luz', montlyCost: 500 },
-  { costName: 'Agua', montlyCost: 200 },
-  { costName: 'Sueldos', montlyCost: 2000 },
-  { costName: 'Gas', montlyCost: 1000 },
+  { id: 1, costName: 'Luz', montlyCost: 500 },
+  { id: 2, costName: 'Agua', montlyCost: 200 },
+  { id: 3, costName: 'Sueldos', montlyCost: 2000 },
+  { id: 4, costName: 'Gas', montlyCost: 1000 },
 ];
 
 async function seed() {
-  await Promise.all(fixedCosts.map(fixedCost => db.fixedCost.create({ data: fixedCost })));
+  for (const fixedCost of fixedCosts) {
+    await db.fixedCost.upsert({
+      where: { id: fixedCost.id },
+      update: fixedCost,
+      create: fixedCost,
+    });
+  }
 }
 
 seed();
